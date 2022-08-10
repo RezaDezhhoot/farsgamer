22b66700417d4860100a9a49fd50f4395ead68a7
reza <rdezhhoot@gmail.com>
2022-08-10 13:52:08 +0430

    edit lfm midleware

--- routes/web.php
+++ routes/web.php
@@ -75,7 +75,7 @@ Route::middleware(['auth','role:admin'])->namespace('App\Http\Livewire\Admin')->
     Route::get('/settings/fag/{action}/{id?}', Settings\CreateFag::class)->name('admin.setting.fag.create');
 });
 // file manager
-Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['auth','role:admin']], function () {
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['auth:web']], function () {
     \UniSharp\LaravelFilemanager\Lfm::routes();
 });
 // logout
